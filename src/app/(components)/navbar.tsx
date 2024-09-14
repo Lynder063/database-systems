e953e6b0f891e9f49c8baa97798386d71e1d6702
Lynder063 <lynderjekokot@gmail.com>
2024-09-15 00:06:16 +0200

    Updating DatabaseIcon

--- src/app/(components)/navbar.tsx
+++ src/app/(components)/navbar.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import DatabaseIcon from "./databseicon";
 
 export default function Navbar() {
   return (
@@ -44,26 +45,5 @@ export default function Navbar() {
         </nav>
       </div>
     </header>
-  );
-}
-
-function DatabaseIcon(props) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <ellipse cx="12" cy="5" rx="9" ry="3" />
-      <path d="M3 5V19A9 3 0 0 0 21 19V5" />
-      <path d="M3 12A9 3 0 0 0 21 12" />
-    </svg>
   );
 }

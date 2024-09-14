58c2b90732099ec96311984e45d92d11863577b7
Lynder063 <lynderjekokot@gmail.com>
2024-09-15 00:39:40 +0200

    Trying fixing actions

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,15 @@
+// next.config.js
 import createMDX from "@next/mdx";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
+  basePath: "/database-systems",
+  assetPrefix: "/database-systems/",
+  images: {
+    unoptimized: true,
+  },
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
-
-  // Optionally, add any other Next.js config below
+  // Remove output: "export"
 };
 
 const withMDX = createMDX({

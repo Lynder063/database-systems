823b97298a6ffe17ff33a28eeed042c6a39e8153
Lynder063 <lynderjekokot@gmail.com>
2024-09-14 23:51:15 +0200

    Creating main page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,100 +1,133 @@
-import Image from "next/image";
+import Link from "next/link";
+import "highlight.js/styles/atom-one-dark.css";
+import Navbar from "./(components)/navbar";
 
-export default function Home() {
+export default function Homepage() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="flex flex-col min-h-screen bg-gray-300">
+      <Navbar />
+      <main className="flex-1 text-black">
+        <section id="pravni" className="py-12 md:py-24">
+          <div className="bg-red-800 text-red-200 container grid items-center gap-2 px-10 mx-auto md:px-6 lg:grid-cols-1 lg:gap-1 rounded-xl p-10">
+            <div className="space-y-2">
+              <h2 className="text-3xl font-bold tracking-tighter md:text-4xl/tight text-center">
+                Právní upozornění
+              </h2>
+              <p className="md:text-xl lg:text-base xl:text-xl text-center">
+                Obsah na této webové stránce je poskytován výhradně pro
+                edukativní účely. Informace zde uvedené nemusí nutně odrážet
+                skutečnou realitu a mohou obsahovat vymyšlené nebo smyšlené
+                prvky. Jakákoli podobnost s reálnými osobami, událostmi nebo
+                místy je čistě náhodná. Autor a provozovatel této stránky
+                nepřebírá žádnou zodpovědnost za správnost, úplnost ani
+                aktuálnost informací uvedených na této stránce. Jakékoliv kroky
+                podniknuté na základě informací z této stránky jsou výhradně na
+                vlastní riziko návštěvníka. Doporučujeme si před podniknutím
+                jakéhokoliv kroku ověřit informace z důvěryhodných zdrojů.
+                Použitím této stránky berete na vědomí a souhlasíte s těmito
+                podmínkami.
+              </p>
+            </div>
+          </div>
+        </section>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+        <section id="zadani" className="py-12 md:py-12">
+          <div className="container grid items-center bg-slate-50 p-12 rounded-lg justify-center gap-6 px-4 mx-auto md:px-6 lg:grid-cols-2 lg:gap-10">
+            <div className="space-y-2">
+              <h2 className="text-3xl font-bold tracking-tighter md:text-4xl/tight">
+                Příklady
+              </h2>
+              <p className="max-w-[600px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+                Podívejte se na naše příklady a ukázky z oblasti databázových
+                systémů.
+              </p>
+            </div>
+            <div className="flex flex-col gap-2 min-[400px]:flex-row lg:justify-end">
+              <Link
+                href="/priklady"
+                className="inline-flex h-10 items-center justify-center rounded-md bg-primary px-8 text-sm font-medium hover:text-white hover:font-semibold shadow text-black hover:bg-black focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
+                prefetch={false}
+              >
+                Zobrazit příklady
+              </Link>
+            </div>
+          </div>
+        </section>
+        <section id="vysledky" className="py-12 md:py-12">
+          <div className="container grid items-center bg-slate-50 p-12 rounded-lg justify-center gap-6 px-4 mx-auto md:px-6 lg:grid-cols-2 lg:gap-10">
+            <div className="space-y-2">
+              <h2 className="text-3xl font-bold tracking-tighter md:text-4xl/tight">
+                Výsledky
+              </h2>
+              <p className="max-w-[600px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+                Podívejte se na výsledky příkladů.
+              </p>
+            </div>
+            <div className="flex flex-col gap-2 min-[400px]:flex-row lg:justify-end">
+              <Link
+                href="/vysledky"
+                className="inline-flex h-10 items-center justify-center rounded-md bg-primary px-8 text-sm font-medium hover:text-white hover:font-semibold shadow text-black hover:bg-black focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
+                prefetch={false}
+              >
+                Zobrazit výsledky
+              </Link>
+            </div>
+          </div>
+        </section>
+        <section id="teorie" className="py-12 md:py-12">
+          <div className="container grid items-center bg-slate-50 p-12 rounded-lg justify-center gap-6 px-4 mx-auto md:px-6 lg:grid-cols-2 lg:gap-10">
+            <div className="space-y-2">
+              <h2 className="text-3xl font-bold tracking-tighter md:text-4xl/tight">
+                Teorie
+              </h2>
+              <p className="max-w-[600px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+                Prostudujte si teoretické materiály a informace k předmětu
+                Databázové systémy.
+              </p>
+            </div>
+            <div className="flex flex-col gap-2 min-[400px]:flex-row lg:justify-end">
+              <Link
+                href="/teorie"
+                className="inline-flex h-10 items-center justify-center rounded-md bg-primary px-8 text-sm font-medium hover:text-white hover:font-semibold shadow text-black hover:bg-black focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
+                prefetch={false}
+              >
+                Zobrazit teorii
+              </Link>
+            </div>
+          </div>
+        </section>
+        <section id="teorie" className="py-12 md:py-12">
+          <div className="container grid items-center bg-slate-50 p-12 rounded-lg justify-center gap-6 px-4 mx-auto md:px-6 lg:grid-cols-2 lg:gap-10">
+            <div className="space-y-2">
+              <h2 className="text-3xl font-bold tracking-tighter md:text-4xl/tight">
+                Otázky
+              </h2>
+              <p className="max-w-[600px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+                Prostudujte si teoretické otázky k předmětu Databázové systémy.
+              </p>
+            </div>
+            <div className="flex flex-col gap-2 min-[400px]:flex-row lg:justify-end">
+              <Link
+                href="/otazky"
+                className="inline-flex h-10 items-center justify-center rounded-md bg-primary px-8 text-sm font-medium hover:text-white hover:font-semibold shadow text-black hover:bg-black focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
+                prefetch={false}
+              >
+                Zobrazit otázky
+              </Link>
+            </div>
+          </div>
+        </section>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+      <footer className="bg-muted p-6 md:py-10 w-full text-gray-50 bg-gray-400">
+        <div className="flex flex-row max-w-7xl mt-6 text-md font-semibold ">
+          <p className="text-center">
+            Pokud jste našli nějakou chybu, prosím,{" "}
+            <Link href="#" className="underline" prefetch={false}>
+              otevřete issue na GitHubu
+            </Link>
+            .
+          </p>
+        </div>
       </footer>
     </div>
   );

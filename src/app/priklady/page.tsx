5d003ff9705a8f373253b51b9d428fe5d58547cb
Lynder063 <lynderjekokot@gmail.com>
2024-09-14 23:51:53 +0200

    Adding all subpages with correct rendering

---
+++ src/app/priklady/page.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import Link from "next/link";
+import Priklady from "../md-files/priklady.mdx";
+import "highlight.js/styles/atom-one-dark.css";
+import Navbar from "../(components)/navbar";
+import Footbar from "../(components)/footer";
+
+export default function PrikladyComp() {
+  return (
+    <div className="flex flex-col min-h-screen bg-gray-300">
+      <Navbar />
+      <main className="flex items-center justify-center min-h-screen text-black px-4 sm:px-6 lg:px-8">
+        <div className="prose prose-2xl text-center max-w-4xl">
+          <Priklady />
+        </div>
+      </main>
+
+      <Footbar />
+    </div>
+  );
+}
---
+++ src/app/teorie/page.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import Link from "next/link";
+import Footbar from "../(components)/footer";
+import Teorie from "../md-files/teorie.mdx";
+import "highlight.js/styles/atom-one-dark.css";
+import Navbar from "../(components)/navbar";
+
+export default function TeorieComponent() {
+  return (
+    <div className="flex flex-col min-h-screen bg-gray-300">
+      <Navbar />
+      <main className="flex items-center justify-center min-h-screen text-black px-4 sm:px-6 lg:px-8">
+        <div className="prose prose-2xl text-center max-w-4xl">
+          <Teorie />
+        </div>
+      </main>
+
+      <Footbar />
+    </div>
+  );
+}

eb9953d4259c1ed0add8509b3afacd0d65ec32d9
Lynder063 <lynderjekokot@gmail.com>
2024-09-15 00:19:17 +0200

    Trying a fix from Github

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ import createMDX from "@next/mdx";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  images: {
+    unoptimized: true,
+  },
   // Optionally, add any other Next.js config below
 };
 

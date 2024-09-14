0b5e20c0415dafb97833e4095a1d9c1c24e95ee5
Lynder063 <lynderjekokot@gmail.com>
2024-09-14 23:56:55 +0200

    Forgot to add output

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,8 @@ import createMDX from "@next/mdx";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
+  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],¨
+  output: "export",
   // Optionally, add any other Next.js config below
 };
 

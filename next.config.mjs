034caa0ed614e1b92325a7d4d215ecfb286b39e3
Lynder063 <lynderjekokot@gmail.com>
2024-09-15 14:54:55 +0200

    Tmp PWA setting

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 // next.config.js
 import createMDX from "@next/mdx";
+import withPWAInit from "@ducanh2912/next-pwa";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -10,8 +11,15 @@ const nextConfig = {
   // Remove output: "export"
 };
 
+const withPWA = withPWAInit({
+  dest: "public",
+  cacheOnFrontendNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+});
+
 const withMDX = createMDX({
   // Add markdown plugins here, as desired
 });
 
-export default withMDX(nextConfig);
+export default withPWA(withMDX(nextConfig));
